fece193c5219ff8c351a87a9739ab5c48d312b36
Miyayu <yuchan2215@gmail.com>
2022-12-07 15:47:53 +0900

    change: Helpコマンドのフォーマットを修正

--- src/main/java/com/jonahseguin/drink/command/DrinkHelpService.java
+++ src/main/java/com/jonahseguin/drink/command/DrinkHelpService.java
@@ -1,12 +1,16 @@
 package com.jonahseguin.drink.command;
 
-import lombok.Getter;
-import lombok.Setter;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+
+import java.util.Arrays;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -21,11 +25,18 @@ public class DrinkHelpService {
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7&m--------------------------------"));
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bHelp &7- &6/" + container.getName()));
             for (DrinkCommand c : container.getCommands().values()) {
-                TextComponent msg = new TextComponent(net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&',
-                        "&7/" + container.getName() + (c.getName().length() > 0 ? " &e" + c.getName() : "") + " &7" + c.getMostApplicableUsage() + " &7- &f" + c.getShortDescription()));
-                msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(ChatColor.GRAY + "/" + container.getName() + " " + c.getName() + " - " + ChatColor.WHITE + c.getDescription())));
-                msg.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/" + container.getName() + " " + c.getName()));
-                sender.spigot().sendMessage(msg);
+                TextComponent[] msgArray = {
+                        new TextComponent(net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&',
+                                "&7" + (c.getName().length() > 0 ? " &e" + c.getName() : "/" + container.getName()) + " &7" + c.getMostApplicableUsage() + "&7")),
+                        new TextComponent(net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&',
+                                "&7 ┗━ &f" + c.getShortDescription()))
+                };
+                Arrays.stream(msgArray).forEach(msg -> {
+                    msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(ChatColor.GRAY + "/" + container.getName() + " " + c.getName() + " - " + ChatColor.WHITE + c.getDescription())));
+                    msg.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/" + container.getName() + " " + c.getName()));
+                    sender.spigot().sendMessage(msg);
+                });
+
             }
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7&m--------------------------------"));
         };
